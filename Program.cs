4d0f6d1e96abd986fd979765e7db1c7922f659ef
Mariam Khattab <116390513+N00ne-00@users.noreply.github.com>
2022-12-18 23:01:10 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your number : ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter your number : ");
+            int num2 = int.Parse(Console.ReadLine());
+
+
+            for (int input = num1; input <= num2; input++)
+            {
+                if (input <= 1)
+                {
+
+                    continue;
+                }
+                else
+                {
+                    int x = 0;
+                    for (int i = 2; i < input; i++)
+                    {
+                        if (input % i == 0)
+                        {
+                            x = 1;
+                            break;
+                        }
+
+
+
+                    }
+                    if (x == 0)
+                    {
+                        Console.WriteLine(input + " is a prime number");
+                    }
+
+
+                }
+            }
+
+        }
+    }
+}
+
